665b201f805dc7391e7def916f8d2cdc916b14ad
joannaB2 <jbu8809@gmail.com>
2023-10-27 20:29:39 +0200

    express routing, serving files

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-const http = require('http');
-const routes = require('./routes');
+const express = require('express');
+const path = require('path');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+
+//parser for every response body
+const bodyParser = require('body-parser');
+
 /* streams 
     node is parsing streams (req) in chunks
 */
@@ -8,6 +16,16 @@ const routes = require('./routes');
     allows to work on multiple chunks at once and release them when finished
 */
 
-const server = http.createServer(routes);
+const app = express();
 
-server.listen(3000);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', 'page-not-found.html'));
+});
+
+app.listen(3000);
